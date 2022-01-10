02b59287d4094b6f8560be295e534f863467ba03
geoallenrh <geoffallen@redhat.com>
2022-01-10 14:32:13 -0600

    trigger me

--- src/main/java/com/redhat/geoallen/GreetingResource.java
+++ src/main/java/com/redhat/geoallen/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Happy 2022";
+        return "Happy 2022 - please trigger";
     }
 }
